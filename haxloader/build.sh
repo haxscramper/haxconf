03a537e5e44630daa22780575059922ccd2fd9c7
haxscramper <haxscramper@gmail.com>
2022-02-12 14:57:32 +0300

    write emacs modules in nim

---
+++ haxloader/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# bash
+set -o nounset
+set -o errexit
+
+function gdb_cmd {
+    gdb                                           \
+        -batch                                    \
+        -ex "set print address off"               \
+        -ex "set print frame-arguments presence"  \
+        -ex "run"                                 \
+        -ex "bt"                                  \
+        --args $@
+}
+
+nim c -o=haxloader.so haxloader.nim
+emacs -Q --batch --directory "$PWD" -l main.el
