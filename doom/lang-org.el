d585d51e1d048da6bf97673b02ecd8bd0e8fdf21
haxscramper <haxscramper@gmail.com>
2024-07-26 15:32:44 +0400

    emacs: enable org indent mode by default

--- doom/lang-org.el
+++ doom/lang-org.el
@@ -1029,7 +1029,7 @@ the empty area."
   (setq flyspell-generic-check-word-predicate 'hax/flyspell-org-mode-verify)
   (abbrev-mode 1)
   (flyspell-mode 1)
-  (org-indent-mode -1)
+  (org-indent-mode t)
   ;; Indentation guides slow down org-mode when there are multiple folds
   ;; (at least I was able to identifiy the implementation ot that point)
   ;; (highlight-indent-guides-mode -1)
@@ -1921,6 +1921,8 @@ If OTHERS is true, skip all entries that do not correspond to TAG."
         (goto-char (org-end-of-subtree current-subtree))
       nil)))
 
+
+(setq org-startup-indented t)
 
 (defun hax/org-agenda-skip ()
   (or (hax/org-has-tag "no_agenda")
